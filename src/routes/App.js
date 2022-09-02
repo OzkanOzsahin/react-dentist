19ecac4904e5c5bd130cb1d8237e7c1462f9173f
OzkanOzsahin <ozkanozsahin@hotmail.com>
2022-09-02 14:58:15 +0200

    new updates

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,48 +1,35 @@
 import React from "react";
 import "./App.css"
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "../views/Home";
 //import Calendar from "../views/Calendar";
 import Day from "../views/Day";
 import { getInitialState } from "../utils/utils";
+import Calendar from "../views/Calendar";
 
-const { appointments } = getInitialState();
-
+const { appointments }  = getInitialState();
 
 const App = () => (
     <Router>
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-
-                        <Link to="/calendar">Calendar view</Link>
-                    </li>
-                    <li>
-                        <Link to="/day">Day view</Link>
-                    </li>
-                </ul>
-            </nav>
-            <main>
-                <Routes>
-                    <Route path="/calendar">
-                    </Route>
-                    <Route path="/day" element={<Day appointments={appointments.filter(app => app.day === 1)} />}>
-                        
-                    </Route>
-                    <Route path="/" element={<Home/>}>
-                        
-                    </Route>
-                </Routes>
-            </main>
-        </div>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/day">Day</Link></li>
+          <li><Link to="/calendar">Calendar</Link></li>
+        </ul>
+  
+        <hr />
+  
+        <Route exact path="/" component={Home} />
+        <Route path="/day" component={Day} />
+        <Route path="/calendar" component={Calendar} />
+      </div>
     </Router>
-);
+  );
 
 export default App;
+
+
 
 
 
